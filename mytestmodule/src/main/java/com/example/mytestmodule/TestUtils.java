74d5d7bb19636c0a252b910e0a9e3ecd929fac85
799782239 <799782239@qq.com>
2017-07-24 14:26:41 +0800

    Initial commit

---
+++ mytestmodule/src/main/java/com/example/mytestmodule/TestUtils.java
@@ -0,0 +1,11 @@
+package com.example.mytestmodule;
+
+/**
+ * Created by yanqijs on 2017/7/24.
+ */
+
+public class TestUtils {
+    public String outPut(String string) {
+        return string;
+    }
+}
